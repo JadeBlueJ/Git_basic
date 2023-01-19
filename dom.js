818193eb4ea5fc9a9a1db1f85f8e710c90b02847
JadeBlueJ <jayjeetchamp@gmail.com>
2023-01-20 00:28:55 +0530

    Task 3

---
+++ dom.js
@@ -0,0 +1,4 @@
+ 
+(document.getElementById('main-header').style.border='solid 3px #000')  
+document.getElementById('title').innerHTML='<b> Add Items </b>'
+document.getElementById('title').style.color='green'
