84a3a3c7c686c59be8da0fd1ff0ef0e51d8395fa
JadeBlueJ <jayjeetchamp@gmail.com>
2023-01-20 16:31:48 +0530

    Task commit

--- dom.js
+++ dom.js
@@ -2,3 +2,13 @@
 (document.getElementById('main-header').style.border='solid 3px #000')  
 document.getElementById('title').innerHTML='<b> Add Items </b>'
 document.getElementById('title').style.color='green'
+
+let items = document.getElementsByClassName('list-group-item');
+
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++)
+items[i].style.fontWeight='bold'
+
+
+
