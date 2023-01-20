c1e3ec87c13f8b50113d98b685af556cfe5c0741
JadeBlueJ <jayjeetchamp@gmail.com>
2023-01-20 23:29:06 +0530

    commit task 6

--- dom.js
+++ dom.js
@@ -3,10 +3,18 @@ document.getElementById('title').innerHTML='<b> Add Items </b>'
 document.getElementById('title').style.color='green'
 
 
-let items = document.getElementsByTagName('li');
+//let items = document.getElementsByTagName('li');
 
-items[2].style.backgroundColor='green';
+/*items[2].style.backgroundColor='green';
 
 for(var i=0;i<items.length;i++)
 {items[i].style.fontWeight='bold'
-items[i].style.background='#f4f4f4'}
+items[i].style.background='#f4f4f4'}*/
+
+
+var item2 = document.querySelectorAll('.list-group-item');
+
+
+item2[1].style.backgroundColor='green';
+
+item2[2].style.opacity='0.0';
