5589414fc0fa091c39318cd720c1cfd644edb289
JadeBlueJ <jayjeetchamp@gmail.com>
2023-01-29 13:47:06 +0530

    task 14 commit

--- main.js
+++ main.js
@@ -1,70 +1,62 @@
+
 function getval(event){
     event.preventDefault();
     alert("The form has been submitted");
-const nam = document.getElementById('fname').value;
-const mail = document.getElementById('mail').value;
-const phone = document.getElementById('phone').value;
-//const dat=document.getElementById('call').value;
-//const tim =document.getElementById('tim').value;
-//localStorage.setItem('name',nam);
-//localStorage.setItem('mail',mail);
-//localStorage.setItem('date',dat);
-//localStorage.setItem('phone',phone);
-//localStorage.setItem('time',tim);
+    var name1 = event.target.fname.value;
+    var mail = event.target.mail.value;
+    var phone = event.target.phone.value;
+
 let ob = {
-    name : nam,
-    mail : mail,
-    phone : phone,
-    //date : dat,
-    //time : tim,
+    name1,
+    mail,
+    phone
 };
 
 localStorage.setItem(mail, JSON.stringify(ob))
+UIelement(ob);
 
 }
-
-const form = document.querySelector('#my-form');
-const nam = document.querySelector('#fname');
-const email = document.querySelector('#mail');
-const phon = document.querySelector('#tel')
 const ul = document.querySelector('#users');
-//const msg = document.querySelector('.msg');
 var itemlist = document.querySelector('.users');
 
-document.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    /*if(nam.value==='' || email.value==='')
-    {
-        msg.classList.add('error');
-        msg.innerHTML='Please fill out the necessary fields'
+function UIelement(ob){
 
-        setTimeout(() => {
-            msg.remove();
-        }, 3000);
-    }
-    else */
-        const li=document.createElement('li');
-        li.appendChild(document.createTextNode(`${nam.value} : ${email.value}`));
-        ul.appendChild(li);
-        nam.value='';
-        email.value='';
-        phone.value='';
+        var li=document.createElement('li');
+        li.appendChild(document.createTextNode(`${ob.name1} : ${ob.mail} : ${ob.phone}`) );
+        li.id=ob.mail;
+        //console.log(li);
+
         
         var newele=document.createElement('button');
         newele.className=' btn btn-danger btn-sm m-1 float-right';
         newele.appendChild(document.createTextNode('Delete'));
         newele.id='delbtn';
-
         newele.onclick =() =>
-        {
-            if (confirm('delete me'))
-            {
+        {   
+            if (confirm('delete me?'))
+            {   console.log(li)
+                localStorage.removeItem(ob.mail)
                 ul.removeChild(li);
-                //itemlist.removeChild(delitem);
+                // why does this work? ul.removeChild(li);
             }
         }
+
+        var editele = document.createElement('button');
+        editele.className='btn btn-secondary btn-sm m-1 float-right'
+        editele.appendChild(document.createTextNode('Edit'));
+        editele.onclick=()=>{
+            localStorage.removeItem(ob.mail);
+            document.getElementById('fullname').value = ob.name1;
+            document.getElementById('mailid').value = ob.mail;
+            document.getElementById('phoneno').value = ob.phone;
+            
+            ul.removeChild(li)
+        }
+
         li.appendChild(newele);
-})
+        li.appendChild(editele);
+        ul.appendChild(li);
+}
 
 
 
