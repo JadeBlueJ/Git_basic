784abf34045b9326c6167085fe79c51ee45ce7b1
JadeBlueJ <jayjeetchamp@gmail.com>
2023-03-15 22:27:53 +0530

    Backend_task_2

--- Backend_app_2/app.js
+++ Backend_app_2/app.js
@@ -1,19 +1,27 @@
 const http = require('http')
+const bodyParser = require('body-parser')
 
 const exp = require('express');
 
 const app = exp()
 
-app.use((req,res,next)=>{
-    console.log('inside first mw');
-    next();
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><input type = "text" name = "size"><button type = "submit">Add Prod</button></form>')
 })
 
-app.use((req,res,next)=>{
-    console.log('inside 2nd mw');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express</h1>')
-
 })
+
+
 // const server = http.createServer(app);
 
 // server.listen(4000)
