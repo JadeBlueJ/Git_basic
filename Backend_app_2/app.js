a2da0670b17e0fac58da1dde1f4ca20da52d2dfd
JadeBlueJ <jayjeetchamp@gmail.com>
2023-03-15 23:05:23 +0530

    Backend_Task3

--- Backend_app_2/app.js
+++ Backend_app_2/app.js
@@ -1,5 +1,8 @@
 const http = require('http')
+
 const bodyParser = require('body-parser')
+const admin_routes = require('./routes/admin')
+const shop_routes = require('./routes/shop')
 
 const exp = require('express');
 
@@ -7,24 +10,16 @@ const app = exp()
 
 app.use(bodyParser.urlencoded({extended:false}))
 
+app.use('/admin',admin_routes)
+app.use('/shop',shop_routes)
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><input type = "text" name = "size"><button type = "submit">Add Prod</button></form>')
+app.use((req,res,next)=>{
+    res.status('404').send('<h1>Error 404: Page not Found</h1>')
 })
-
-app.post('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello from Express</h1>')
-})
-
 
 // const server = http.createServer(app);
-
 // server.listen(4000)
+ 
 app.listen(4000)
 
 
