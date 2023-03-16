f663711faa4d62f615fc48119da0942a913e98c0
JadeBlueJ <jayjeetchamp@gmail.com>
2023-03-16 22:06:27 +0530

    Basic_group_chat_express

--- GroupChat/app.js
+++ GroupChat/app.js
@@ -1,4 +1,7 @@
 const http = require('http')
+const fs = require('fs')
+// const localStorage = require('node-localstorage').LocalStorage
+
 
 const bodyParser = require('body-parser')
 const admin_routes = require('./routes/admin')
@@ -10,9 +13,46 @@ const app = exp()
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/login',login_routes)
-// app.use('/admin',admin_routes)
 
+//login page
+app.use('/login',(req,res,next)=>{
+    res.send('<form onsubmit="localStorage.setItem(`username`,document.getElementById(`username`).value)" action = "/home" method = "POST"><input type = "text" id = "username" name = "title"><button type = "submit">Enter username</button></form>')
+    
+})
+
+//check if chats present before hand or else respond with a "send message" form. 
+app.use('/home',(req,res,next)=>{
+    fs.readFile('msg.txt',(e,data)=>{
+        if(data.length==0)
+        {
+            res.setHeader('Content-Type', 'text/html')
+            res.write(`<body>No chats Yet</body>`)
+            res.write(' <form action="/chat" method="POST" onsubmit="document.getElementById(`uname`).value=localStorage.getItem(`username`)"><input type = "text" id = "msg" name = "message"><input type = "hidden" id = "uname" name = "username"><button type = "submit">Enter Your Message</button></form>')
+            res.write('</html>')
+            return res.end();
+        }
+        res.send(
+            '<form action="/chat" method="POST" onsubmit="document.getElementById(`uname`).value=localStorage.getItem(`username`)"><input type = "text" id = "msg" name = "message"><input type = "hidden" id = "uname" name = "username"><button type = "submit">Enter Your Message</button></form>')
+
+    })
+        
+})
+
+// Here we read from the file and post the chat string and then wait for the next request from client
+app.use('/chat',(req,res,next)=>{
+    fs.appendFile('msg.txt',`${req.body.username} :${req.body.message}`,()=>{
+        fs.readFile('msg.txt',(e,data)=>{
+            res.setHeader('Content-Type', 'text/html')
+            res.write(`<body>${data}</body>`)
+            res.write(' <form action="/chat" method="POST" onsubmit="document.getElementById(`uname`).value=localStorage.getItem(`username`)"><input type = "text" id = "msg" name = "message"><input type = "hidden" id = "uname" name = "username"><button type = "submit">Enter Your Message</button></form>')
+            res.write('</html>')
+            return res.end();
+
+        })
+    });
+    
+   
+})
 
 app.use((req,res,next)=>{
     res.status('404').send('<h1>Error 404: Page not Found</h1>')
--- GroupChat/routes/login.js
+++ GroupChat/routes/login.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const localStorage = require('node-localstorage')
 
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
-
-    console.log(req.body);
-    localStorage.setItem(req.body);
+    console.log(res);
     res.send('<form action = "/" method = "POST"><input type = "text" name = "username"><button type = "submit">Enter username</button></form>')
-
-    res.redirect('/')
+    
 })
+
+// router.post('/',(req,res,next)=>{
+//     console.log(req.body)
+//     localStorage.setItem(req.body);
+//     res.redirect('/')
+// })
 
 
 module.exports = router
