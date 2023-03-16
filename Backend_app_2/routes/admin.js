ea9db1ff94ef3d131dbecededb41af529d5e08ef
JadeBlueJ <jayjeetchamp@gmail.com>
2023-03-17 00:04:04 +0530

    correction commit

--- Backend_app_2/routes/admin.js
+++ Backend_app_2/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express')
-
+const path = require('path')
 const router = express.Router();
 
+
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action = "/admin/add-product" method = "POST"><input type = "text" name = "title"><input type = "text" name = "size"><button type = "submit">Add Prod</button></form>')
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
 })
 
 router.post('/add-product',(req,res,next)=>{
