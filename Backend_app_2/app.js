0007985e48214eb4639888da53ddc0c3d045118e
JadeBlueJ <jayjeetchamp@gmail.com>
2023-03-22 00:00:08 +0530

    SQL_files

--- Backend_app_2/app.js
+++ Backend_app_2/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db =require('./util/database')
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+db.execute('select & from products')
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,4 +24,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(4000);
+app.listen(3000);
--- Backend_app_2/controllers/admin.js
+++ Backend_app_2/controllers/admin.js
@@ -4,48 +4,55 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing:false  
+    editing: false
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const id = null;
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(id,title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit
-  if(!editMode) 
-    return res.redirect('/')
-  const productID = req.params.productID
-  Product.findbyID(productID,product=>{
-    if(!product) 
-      res.redirect('/')
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect('/');
+    }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
-      editing:editMode,
+      editing: editMode,
       product: product
+    });
   });
-});
-}
+};
 
 exports.postEditProduct = (req, res, next) => {
-  const prodID = req.body.productID
-  const updateTitle = req.body.title;
-  const updatedImageUrl = req.body.imageUrl;
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedDescription = req.body.description;
-  const updatedProduct = new Product(prodID,updateTitle,updatedImageUrl,updatedPrice,updatedDescription)
-  updatedProduct.save()
-  res.redirect('/admin/products')
-}
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -57,9 +64,8 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.deleteProduct = (req,res,next)=>{
-  const productID = req.params.productID
-  Product.deleteID(productID)
-  res.redirect('/admin/products')
-
-}
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
+};
