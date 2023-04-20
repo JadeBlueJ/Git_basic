5f6c3a874dbb8b9bfb8c01df52312af11218bf51
JadeBlueJ <jayjeetchamp@gmail.com>
2023-04-20 22:09:32 +0530

    Expense_Deployment

--- Expense_Tracker_Live/app.js
+++ Expense_Tracker_Live/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const fs =require('fs')
 
 const bodyParser = require('body-parser');
--- Expense_Tracker_Live/util/database.js
+++ Expense_Tracker_Live/util/database.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize')
-require('dotenv').config()
-const DBpwd = process.env.DB_PASSWORD
-const host = process.env.HOST
-// console.log(DBpwd)
-const sequelize = new Sequelize('expense_app','root',DBpwd,
-{dialect:'mysql',host:host})
+// require('dotenv').config()
+
+const sequelize = new Sequelize(process.env.DB_NAME,'root',process.env.DB_PASSWORD,
+{dialect:'mysql',host:process.env.HOST})
 
  module.exports=sequelize
